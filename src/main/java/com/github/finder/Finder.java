3494141051e76d76a15afe30dfa7cdd531ce85be
wazyu <g1344630@cc.kyoto-su.ac.jp>
2015-07-01 17:31:55 +0900

    refs #8 コマンドラインオプションで size オプションが指定された場合の処理を追加する
    コマンドラインオプションでsizeオプションが指定された場合の処理を追加した.

--- src/main/java/com/github/finder/Finder.java
+++ src/main/java/com/github/finder/Finder.java
@@ -36,7 +36,7 @@ public class Finder {
         }
     }
 
-    private boolean isTarget(File file){
+    /* private boolean isTarget(File file){
         boolean flag = true;
         if(args.getName() != null){
             flag &= checkTargetName(file, args.getName());
@@ -45,7 +45,7 @@ public class Finder {
             flag &= checkTargetType(file, args.getType());
         }
         return flag;
-    }
+	}*/
 
     private boolean checkTargetName(File file, String pattern){
         String name = file.getName();
@@ -62,6 +62,39 @@ public class Finder {
         }
         else if(type.equals("h") || type.equals("hidden")){
             return file.isHidden();
+        }
+        return false;
+    }
+     private boolean isTarget(File file){
+        boolean flag = true;
+        if(args.getName() != null){
+            flag &= checkTargetName(file, args.getName());
+        }
+        if(args.getType() != null){
+            flag &= checkTargetType(file, args.getType());
+        }
+        if(args.getSize() != null){
+            flag &= checkTargetSize(file, args.getSize());
+        }
+
+        return flag;
+    }
+    private boolean checkTargetSize(File file, String sizeString){
+        if(file.isFile()){
+            char sign = sizeString.charAt(0);
+            String string = sizeString.substring(1);
+            int size = Integer.parseInt(string);
+
+            switch(sign){
+            case '>':
+                return file.length() > size;
+            case '<':
+                return file.length() < size;
+            case '=':
+                return file.length() == size;
+            default:
+                // ignore
+            }
         }
         return false;
     }
