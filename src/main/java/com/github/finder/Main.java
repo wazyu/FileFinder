4a09d034d22efeb5fc0f31c8d3f30eb593d0c126
ymsshr <g1344595@cse.kyoto-su.ac.jp>
2015-07-01 14:11:43 +0900

    refs #4   Mainにコマンドラインオプションを解析する処理を追加する．
    Main クラスにコマンドラインオプションを解析する処理（parseArguments(String[] arguments)）を追加した.

--- src/main/java/com/github/finder/Main.java
+++ src/main/java/com/github/finder/Main.java
@@ -1,10 +1,24 @@
 package com.github.finder;
 
+import org.kohsuke.args4j.CmdLineException;
+import org.kohsuke.args4j.CmdLineParser;
+
+
 public class Main{
     public Main(String[] arguments){
     }
 
     public static void main(String[] args){
         new Main(args);
+    }
+
+    private Args parseArguments(String[] arguments){
+        Args args = new Args();
+        try {
+            CmdLineParser parser = new CmdLineParser(args);
+            parser.parseArgument(arguments);
+        } catch (CmdLineException e) {
+        }
+        return args;
     }
 }
